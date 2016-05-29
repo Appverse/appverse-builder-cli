7e03fa6ebf1cecd3b5356f6ad369e20f609104b7
Vila Conesa, Ferran <Ferran.Vila@gft.com>
2016-05-29 07:46:42 +0200

    Fix the whoami test. We are mocking the filesystem, it's not necessary to override the items

--- test/api/apb-whoami.js
+++ test/api/apb-whoami.js
@@ -6,9 +6,10 @@ var lib = common.lib;
 
 it('WHOAMI should be executed without errors', function (done) {
 
-    lib.setUserConfig('access_token', '1234567890');
-    lib.setUserConfig('whoami', 'foo');
-    whoami.run({}, lib, function () {
+    // It's not necessary to create the items, we mocked in the test.js
+    //lib.setUserConfig('access_token', '1234567890');
+    //lib.setUserConfig('whoami', 'foo');
+    whoami.run({}, lib, function() {
         done();
     });
 });
